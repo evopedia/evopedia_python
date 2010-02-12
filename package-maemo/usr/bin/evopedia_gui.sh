272f90ca7120aac1064a3d42753b296f12ea117b
crei <christian@exinto.de>
2010-02-12 14:44:44 +0100

    removed old and unused package directories

--- package-maemo/usr/bin/evopedia_gui.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-HTTP_PORT="8080"
-BROWSER="/usr/bin/browser --full-screen --url="
-[ -f /etc/evopedia.conf ] && . /etc/evopedia.conf
-
-#/etc/init.d/evopedia start
-
-$BROWSER"http://127.0.0.1:$HTTP_PORT"
